2a1bc38ce8213f680e08978d7729dd9bd2148014
Karen Gomes <karenecris12015@gmail.com>
2024-09-24 14:54:32 -0300

    instalação do nodemon, criação de rotas e criação de gitignore para node_modules

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from "http";
 
 const PORT = 3000;
 
+const rotas = {
+    "/": "curso de Node.jS",
+    "/livros": "livros",
+    "/autores": "entrei na rota autores"
+};
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Curso de Node.js");
+  res.end(rotas[req.url]);
 });
 
 server.listen(PORT, () => {

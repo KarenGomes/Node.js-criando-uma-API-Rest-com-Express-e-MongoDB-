d32b0879cbffff9654923f529a12802ab51d7ab8
Karen Gomes <karenecris12015@gmail.com>
2024-09-24 15:30:06 -0300

    Criação de métodos get, post e put de livros

--- src/app.js
+++ src/app.js
@@ -1,9 +1,47 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
+const livros = [
+    {
+        id: 1,
+        titulo: "Senhor dos anéis"
+    },
+    {
+        id: 2,
+        titulo: "jogos vorazes"
+    }
+]
+
+function buscaLivro(id){
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    })
+}
 
 app.get("/", (req, resp) => {
     resp.status(200).send("Curso de Node.js");
+})
+
+app.get("/livros", (req, resp) => {
+    resp.status(200).json(livros);
+})
+
+app.get("/livros/:id", (req, resp) => {
+    const index = buscaLivro(req.params.id);
+    resp.status(200).json(livros[index]);
+})
+
+app.post("/livros", (req, resp) => {
+    livros.push(req.body);
+    resp.status(201).send("Livro cadastrado com sucesso");
+})
+
+app.put("/livros/:id", (req, resp) => {
+    const index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    resp.status(200).json(livros);
 })
 
 export default app;
